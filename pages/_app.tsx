ad290183ea7a9694b0282071d50b1d55f3d3384b
goodweather37 <140213173+goodweather37@users.noreply.github.com>
2023-10-26 11:41:59 +1030

    progress-bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,94 +1,26 @@
+import dynamic from "next/dynamic";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import NProgress from "nprogress";
-import 'nprogress/nprogress.css';
+import '../styles/nprogress.css';
 import "../styles/globals.css";
 import { useEffect, useState } from "react";
 import { Router, useRouter } from "next/router";
-import usePageLoad from "../utils/useLoading";
+
+const ProgressBar = dynamic(() => import('../utils/progress-bar'), { ssr: false });
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
 const activeChain = "mumbai";
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
-
-  //   setTimeout(() => {
-  //     setLoading(false)
-  //   }, 5000)
-
-  // }, []);
-
-  // useEffect(() => {
-  //   const start = () => {
-  //     console.log("start");
-  //     setLoading(true);
-  //   }
-  //   const end = () => {
-  //     console.log("end");
-  //     setLoading(false);
-  //   }
-
-  //   Router.events.on('routeChangeStart', start);
-  //   Router.events.on('routeChangeComplete', end)
-  //   Router.events.on('routeChangeError', end)
-
-  //   return () => {
-  //     Router.events.off('routeChangeStart', start)
-  //     Router.events.off('routeChangeComplete', end)
-  //     Router.events.off('routeChangeError', end)
-  //   }
-  // }, []);
-  // useEffect(() => {
-  //   const start = () => {
-  //     console.log("start");
-  //     setLoading(true);
-  //   };
-  //   const end = () => {
-  //     console.log("findished");
-  //     setLoading(false);
-  //   };
-  //   Router.events.on("routeChangeStart", start);
-  //   Router.events.on("routeChangeComplete", end);
-  //   Router.events.on("routeChangeError", end);
-  //   return () => {
-  //     Router.events.off("routeChangeStart", start);
-  //     Router.events.off("routeChangeComplete", end);
-  //     Router.events.off("routeChangeError", end);
-  //   };
-  // }, []);
-
-  const router = useRouter();
-
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        const handleChangeStart = (url: string) => {
-            if (url === "<root_to_show_loading>") {
-                setIsLoading(true);
-            }
-        };
-
-        const handleChangeEnd = (url: string) => {
-            if (url === "<root_to_show_loading") {
-                setIsLoading(false);
-            }
-        };
-
-        router.events.on("routeChangeStart", handleChangeStart);
-        router.events.on("routeChangeComplete", handleChangeEnd);
-        router.events.on("routeChangeError", handleChangeEnd);
-    }, []);
   return (
     <>
-      {isLoading ? (
-      <p>Loading</p>
-    ) : (
+      <ProgressBar />
       <ThirdwebProvider
       clientId={process.env.CLIENT_ID}
       // sdkOptions={{
@@ -102,7 +34,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
-    )}
     </>
   );
 }
